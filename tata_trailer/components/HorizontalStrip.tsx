fb90e708699f5bea65cf2e0e4531585407b7c656
Advaita <148793147+01adv@users.noreply.github.com>
2024-06-04 22:27:03 +0530

    commit

--- tata_trailer/components/HorizontalStrip.tsx
+++ tata_trailer/components/HorizontalStrip.tsx
@@ -17,34 +17,22 @@ const HorizontalStrip = () => {
           className="text-gray-600 text-4xl font-mono flex items-center justify-center text-nowrap md:justify-start [&_li]:mx-8 [&_img]:max-w-none  animate-infinite-scroll"
         >
           <li>
-            <p className="      tracking-wider">
-              Freight Forwarding
-            </p>
+            <p className="tracking-wider">Freight Forwarding</p>
           </li>
           <li>
-            <p className="      tracking-wider">
-              Cargo Insurance
-            </p>
+            <p className="tracking-wider">Cargo Insurance</p>
           </li>
           <li>
-            <p className="      tracking-wider">
-              Office Moving
-            </p>
+            <p className="tracking-wider">Office Moving</p>
           </li>
           <li>
-            <p className="      tracking-wider">
-              International Shipping
-            </p>
+            <p className="tracking-wider">International Shipping</p>
           </li>
           <li>
-            <p className="      tracking-wider">
-              Ware House Management
-            </p>
+            <p className="tracking-wider">Ware House Management</p>
           </li>
           <li>
-            <p className="      tracking-wider">
-              Logistic Moving
-            </p>
+            <p className="tracking-wider">Logistic Moving</p>
           </li>
         </ul>
       </div>
