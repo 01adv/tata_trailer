48fb0564ef52e9c3b451d249c9c6355f9c81583a
Advaita <148793147+01adv@users.noreply.github.com>
2024-06-26 16:12:23 +0530

    phone number added

--- tata_trailer/components/Footer2.tsx
+++ tata_trailer/components/Footer2.tsx
@@ -16,7 +16,7 @@ const Footer2 = () => {
           <div className="rounded-2xl bg-orange-200 w-full lg:w-2/5 p-8">
             <h2 className="mb-6 text-3xl font-semibold">Connect with us!</h2>
 
-            <table className="mb-4 text-sm text-left text-gray-600">
+            <table className="mb-4 text-xs  sm:text-sm text-left text-gray-600">
               <tbody>
                 <tr className="">
                   <th
@@ -54,6 +54,7 @@ const Footer2 = () => {
                   <td className="px-2 py-2">
                     <p>9761303561, 9058206080</p>
                     <p>9761999097, 8375964014</p>
+                    <p>9639658534</p>
                   </td>
                 </tr>
               </tbody>
@@ -78,18 +79,18 @@ const Footer2 = () => {
 
           <div className="rounded-2xl bg-white w-full lg:w-1/4 p-8">
             <h2 className="mx-auto text-3xl font-semibold tracking-tight sm:text-3xl">
-              Subscribe to our newsletter
+              Keep Connected With Us
             </h2>
 
             <form className="w-full mx-auto mt-6">
               <input
-                id="email-address"
-                name="email"
-                type="email"
-                autoComplete="email"
+                id="phone-number"
+                name="phone"
+                type="phone"
+                autoComplete="phone"
                 required
                 className="w-full rounded-2xl border border-gray-800 bg-white/5 px-3.5 py-2 text-gray-600 text-sm shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
-                placeholder="Enter email"
+                placeholder="Enter Phone Number"
               />
               <Button className="w-full mt-2 rounded-2xl px-3.5 py-2.5 font-semibold shadow-sm">
                 Subscribe
@@ -101,11 +102,11 @@ const Footer2 = () => {
             <div className="flex flex-row justify-between">
               <div>
                 <p className="font-medium">Service</p>
-                <nav className="grid grid-cols-2 mt-4 text-sm text-gray-600">
+                <nav className="grid grid-cols-2 mt-4 text-xs  sm:text-sm text-gray-600">
                   {serviceLinks.map((link, index) => (
                     <Link
                       key={index}
-                      className="hover:opacity-75 py-2"
+                      className="hover:opacity-75 py-2 px-1"
                       href={link.href}
                     >
                       {link.label}
@@ -116,7 +117,7 @@ const Footer2 = () => {
 
               <div>
                 <p className="font-medium">Company</p>
-                <nav className="flex flex-col mt-4 space-y-2 text-sm text-gray-600">
+                <nav className="flex flex-col mt-4 space-y-2 text-xs  sm:text-sm text-gray-600">
                   {companyLinks.map((link, index) => (
                     <Link
                       key={index}
@@ -129,20 +130,6 @@ const Footer2 = () => {
                 </nav>
               </div>
 
-              {/* <div>
-                <p className="font-medium">Legal</p>
-                <nav className="flex flex-col mt-4 space-y-2 text-sm text-gray-600">
-                  {legalLinks.map((link, index) => (
-                    <Link
-                      key={index}
-                      className="hover:opacity-75"
-                      href={link.href}
-                    >
-                      {link.label}
-                    </Link>
-                  ))}
-                </nav>
-              </div> */}
             </div>
             <p className="mt-8 text-sm text-gray-800">
               Copyright © 2024 Tata Trailor Transport, All rights reserved.
